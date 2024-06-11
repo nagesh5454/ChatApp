1b1ebf8bf8455901c1e7626ddb4a6d389cc2d84f
NageshMane <mane46740@gmail.com>
2024-06-11 08:01:40 +0530

    Completed

--- Frontend/src/home/Leftpart/User.jsx
+++ Frontend/src/home/Leftpart/User.jsx
@@ -15,11 +15,14 @@ function User({ user }) {
       onClick={() => setSelectedConversation(user)}
     >
       <div className="flex space-x-4 px-8 py-3 hover:bg-slate-700 duration-300 cursor-pointer">
+
         <div className={`avatar ${isOnline ? "online" : ""}`}>
           <div className="w-12 rounded-full">
-            <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+            <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
           </div>
         </div>
+
+
         <div>
           <h1 className=" font-bold">{user.fullname}</h1>
           <span>{user.email}</span>
